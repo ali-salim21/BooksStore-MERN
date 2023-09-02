c06c48d4686975fb04e5a430db5a70dfb2165eab
Ali Salim <examiner.0005@gmail.com>
2023-09-02 22:29:41 +0400

    added the bookmodel schema and post request

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,54 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from 'mongoose'; // import mongose library after installing by 'npm i mongoose'
+import { Book } from "./models/bookModel.js";
 
-const app = express();
 // Use Express frame work to create our HTTP Route
-// We want to create a new route for / route
+const app = express();
+
+// Middleware for parsing request body
+//app.use(express.json());
+
+// We want to create a new route for "/" route
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Hi MOM! I am MERN Stack student");
 });
+
+// Route to save a new Book
+// since working with mongoos is a async process we will async function
+app.post('/books', async (request, response) => {
+    try {
+        // Quick validation for our input from request.body to be in the requested body,
+        // if NOT we return a response of status 400 & send message for client
+        if (
+            !request.body.title ||
+            !request.body.auther ||
+            !request.body.publisher
+        ) { 
+            return response.status(400).send({
+                message: 'Send all required fields: title, auther, publisher ',
+            });
+        }
+
+        // Create a variable for new book
+        const newBook = {
+            title: request.body.title,
+            author: request.body.auther,
+             publisher: request.body.publisher,
+        };
+        
+        // call book.create to send this newbook to it & save result in the book variable
+        const book = await Book.create(newBook);
+
+        // return a stutus code of 201 and send a book to the client
+        return response.status(201).send(book);
+    } catch (error) {
+        // Receve the error then log the error to the server console.
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 // Connect to mongoose database
 mongoose
---
+++ backend/models/bookModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+// Create book schema variable
+const bookSchema = mongoose.Schema(
+    {
+        title: { type: String, required: true},
+        auther: {type: String, required: true},
+        publisher: {type: Number, required: true},
+    }, // No need for ID since it will be handeled by the mongoose db.
+    
+    { // Time stamp will provide {time of opeartion} and {time of last update}.
+        timestamp: true,
+    }
+);
+
+export const Book = mongoose.model('Cat', bookSchema);
