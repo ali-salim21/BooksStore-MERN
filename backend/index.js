88843035c9b37f55216d0c359504e2c3196a7c53
Ali Salim <examiner.0005@gmail.com>
2023-09-03 13:32:02 +0400

    Use app.get to fetch ONE book mongoDB

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,20 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// Route to get ONE book from database
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch(error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
 // Connect to mongoose database
 mongoose
     .connect(mongoDBURL)
